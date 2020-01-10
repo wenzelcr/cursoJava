4b7de85747fba22fe432917a9dab797f21a55427
Keoma <you@email.com>
2020-01-09 22:33:29 -0300

    commit dos exercicios

---
+++ src/main/java/com/cursoJava/cursoJava/ContaBancaria.java
@@ -0,0 +1,20 @@
+package java.com.cursoJava.cursoJava;
+
+public class ContaBancaria {
+
+    private String codigo;
+    private Double saldo;
+    private Double limite;
+    private Boolean ativa;
+    private int quantidadeSaques;
+    private String nomeCorrentista = "claudio";
+    private char tipoDePessoa;
+
+    float valor = 10f;
+
+    int valorb = (int) valor;
+
+
+    int valorc = Float.floatToIntBits(valor);
+
+}
---
+++ src/main/java/com/cursoJava/cursoJava/Pessoa.java
@@ -0,0 +1,17 @@
+package java.com.cursoJava.cursoJava;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private boolean maiorDeIdade;
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
